70b88be46cc0f673a2017c26fd7cc85fd29360c3
lucasxaviervieira <lucasdavid.xavier@gmail.com>
2024-06-21 17:55:17 -0300

    Feat: Add Navbar and Footer

--- src/static/saved_url/index.js
+++ src/static/saved_url/index.js
@@ -52,7 +52,6 @@ function createContent(data, whatsEl) {
       content.appendChild(span)
       content.classList.add('max-h-36', 'max-w-36')
       // content.innerHTML = '<div style="display: block; width: 144px; height: 144px; background-color: #ccc; border: 1px solid #666; margin: 0 auto;"></div>'
-
     }
   }
 
@@ -83,7 +82,6 @@ function showSaved(data) {
   const savedUrlContainer = document.getElementById('saved-url')
 
   data.forEach(el => {
-    console.log(el)
     const newDiv = createElement(el)
     savedUrlContainer.appendChild(newDiv)
   });
@@ -91,7 +89,6 @@ function showSaved(data) {
 }
 
 function createCopyLinkButton(link) {
-  console.log(link)
   const copyButton = document.createElement('button')
   copyButton.innerHTML = '<svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14 7H16C18.7614 7 21 9.23858 21 12C21 14.7614 18.7614 17 16 17H14M10 7H8C5.23858 7 3 9.23858 3 12C3 14.7614 5.23858 17 8 17H10M8 12H16" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>'
 
